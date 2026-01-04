4a8d390351365b30929cf72660558ca1fdae90ec
samantasubedi <samantasubedi01@gmail.com>
2026-01-04 22:02:31 +0545

    input handling

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,31 @@
+"use client";
+import { useState } from "react";
+
 function Homepage() {
+  const [input, setinput] = useState<string>("");
+  const [task, settask] = useState<string[]>([]);
+  const handleinputchange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const data = e.target.value;
+    setinput(data);
+  };
+  const handleAdd = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!input.trim()) return;
+    settask((prevtask) => [...prevtask, input]);
+    setinput("");
+    console.log(task);
+  };
   return (
     <>
-      <input placeholder="Enter your task"></input>
-      <button>Add</button>
+      <form onSubmit={handleAdd}>
+        <input
+          type="text"
+          value={input}
+          placeholder="Enter your task"
+          onChange={handleinputchange}
+        ></input>
+        <button type="submit">Add</button>
+      </form>
     </>
   );
 }
