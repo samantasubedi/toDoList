f870d9c443c0ae4eae81b6097d57bbbae3ff8c34
samantasubedi <samantasubedi01@gmail.com>
2026-01-20 21:00:48 +0545

    dropdown ui

--- frontend/components/ui/Dropdown.tsx
+++ frontend/components/ui/Dropdown.tsx
@@ -4,13 +4,13 @@ import { useState } from 'react'
 export const Dropdown = () => {
   const[subdropdown,setsubdropdown]=useState(false)
   return (
-    <div className='relative'>
+    <div className='relative z-10'>
     <div className='absolute'>
-<button onClick={()=>{setsubdropdown(!subdropdown)}}>Set Priority</button>
-{ subdropdown && <div className='flex flex-col gap-2'>
-<button>High</button>
-<button>Medium</button>
-<button>Low</button>
+<button className='bg-white p-3 rounded-xl w-40' onClick={()=>{setsubdropdown(!subdropdown)}}>Set Priority</button>
+{ subdropdown && <div className='flex flex-col gap-2 bg-gray-200 p-2 rounded-xl'>
+<button className='bg-red-400 text-white rounded-md'>High</button>
+<button className='bg-amber-400 text-white rounded-md'>Medium </button>
+<button className='bg-emerald-400 text-white rounded-md'>Low</button>
 </div>}
     </div></div>
   )
