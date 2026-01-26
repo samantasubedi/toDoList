9cec41061affeaa038e5e429a505dfdf7d117db0
samantasubedi <samantasubedi01@gmail.com>
2026-01-26 23:48:27 +0545

    responsive ui

--- frontend/components/ui/Empty.tsx
+++ frontend/components/ui/Empty.tsx
@@ -8,7 +8,7 @@ type inputrefrencetype = {
 
 const Empty = ({ inputrefrence }: inputrefrencetype) => {
   return (
-    <div className="flex justify-center mt-15">
+    <div className="flex justify-center mt-15 ml-2 mr-2">
       <div className="flex flex-col gap-5   dark:bg-linear-to-br dark:from-gray-900 dark:to-neutral-900  bg-linear-to-br from-fuchsia-200 to-green-200 p-10 rounded-2xl  ">
         <div className="flex justify-center">
           <Icon
@@ -16,10 +16,10 @@ const Empty = ({ inputrefrence }: inputrefrencetype) => {
             className="text-8xl text-fuchsia-900 dark:text-fuchsia-300"
           />
         </div>
-        <div className="text-6xl textblue-900 text-center font-bold font-serif text-emerald-800 dark:text-emerald-400">
+        <div className="md:text-6xl text-3xl textblue-900 text-center font-bold font-serif text-emerald-800 dark:text-emerald-400">
           All Clear !
         </div>
-        <p className="font-semibold text-gray-600 dark:text-neutral-400">
+        <p className="font-semibold md:text-2xl text-sm text-center text-gray-600 dark:text-neutral-400">
           Your task list is empty. Start planning your next big goal by adding a
           task.
         </p>
@@ -27,7 +27,7 @@ const Empty = ({ inputrefrence }: inputrefrencetype) => {
           onClick={() => {
             inputrefrence.current?.focus();
           }}
-          className=" dark:text-white font-semibold bg-fuchsia-700 hover:shadow-md shadow-fuchsia-900 hover:bg-fuchsia-500 transition-all duration-300 text-lg cursor-pointer h-10"
+          className=" dark:text-white font-semibold  bg-fuchsia-700 hover:shadow-md shadow-fuchsia-900 hover:bg-fuchsia-500 transition-all duration-300 md:text-xl text-md cursor-pointer h-10"
         >
           + Create your First Task
         </Button>
